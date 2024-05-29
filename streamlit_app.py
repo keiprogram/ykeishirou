711bbce68a0d605f69ad18c11e593f9646796c82
keiprogram <100176536+keiprogram@users.noreply.github.com>
2024-05-29 08:42:59 +0000

    ui

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 # Streamlitライブラリをインポート
 import streamlit as st
 
-st.latex("y=3/x")
+# タイトルと説明
+st.title('古文単語ガチャ')
+
+st.write('古文単語をランダムに表示して、勉強をサポートします！')
+st.write('がんばってください！')
+
+# Load the data
+@st.cache
+def load_data():
+    return pd.read_excel("古文単語リスト.xlsx")
+
+st.latex("")
 
 
